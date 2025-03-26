646dba1d3de0993aec75114e191392482e4dafa7
letvarprint <a.brixencov@icloud.com>
2025-03-26 03:43:12 +0200

    Добавил выведение информации во viewDidLoad

---
+++ Homework1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Homework1
+//
+//  Created by Alexandru Brixencov on 25.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let name = "Alex"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Меня зовут \(name), я создал свое первое приложение")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
