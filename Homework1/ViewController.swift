4874a772bcc6cb2a4aa808f504d633ff7b080eee
letvarprint <a.brixencov@icloud.com>
2025-05-22 04:03:56 +0300

    Исправления

--- Homework1/ViewController.swift
+++ Homework1/ViewController.swift
@@ -11,14 +11,14 @@ class ViewController: UIViewController {
     let name = "Alex"
     let surname = "Brixencov"
     var fullName: String {
-            return "\(name) \(surname)"
-        }
-
+        "\(name) \(surname)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Меня зовут \(fullName), я создал свое первое приложение")
     }
-
-
+    
+    
 }
 
